2410a122951c9e1a3ff22d81cc23329e12e9a40d
Vinicius Raphael <133444109+ViniRaphael@users.noreply.github.com>
2024-03-07 21:52:56 -0300

    Add files via upload

---
+++ db_example.sql
@@ -0,0 +1,4 @@
+create database db_example; -- Creates the new database
+create user 'springuser'@'%' identified by 'ThePassword'; -- Creates the user
+grant all on db_example.* to 'springuser'@'%'; -- Gives all privileges to the new user on the newly created database
+grant select, insert, delete, update on db_example.* to 'springuser'@'%';
